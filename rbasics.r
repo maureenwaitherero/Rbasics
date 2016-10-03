483fc3114f133219cdb8f935b029a93fc719110c
maureenwaitherero <waithereromaureen@gmail.com>
2016-10-03 12:55:12 +0300

    import data

--- rbasics.r
+++ rbasics.r
@@ -13,6 +13,15 @@ setwd("~/Documents/R-Analytics") # set working directory
 #******************import data*****************************
 #***********************************************************
 
+#load using file.choose() 
+
+csvdata <- read.csv(file.choose(),header = TRUE) #header = T, implies that first row are variable names
+txtdata <- read.delim(file.choose(),header = TRUE) 
+
+#load using file.choose() and read.table()
+
+csvdata <-read.table(file.choose(),header = T, sep = ",") #csv data
+txtdata <- read.table(file.choose(), header = T, sep="\t") #tabdelimited data 
 
 #******************save work space *****************************
 #***********************************************************
@@ -23,8 +32,6 @@ save.image("basics.Rdata") # this will be saved in current working directory
 #***********************************************************
 
 load("basics.Rdata")
-
 #or use file.choose()command
-
 load(file.choose())
 
